d1803dadf77a651cf83af16c25d5a995a6456522
Gevorg Nakhapetyan <ngs22071993@gmail.com>
2020-12-16 19:08:32 +0300

    feat[ui]: added button to dialog & callback fun

--- webapp/modules/main/controller/HelloPanel.controller.js
+++ webapp/modules/main/controller/HelloPanel.controller.js
@@ -4,8 +4,8 @@ sap.ui.define([
 	'sap/ui/core/Fragment'
 ], function (Controller, MessageToast, Fragment) {
     'use strict';
-    return Controller.extend('mainmodel.controller.HelloPanel', {
-        onShowHello : function () {
+    return Controller.extend('webapp.modules.main.controller.HelloPanel', {
+        onShowHello: function () {
             // читаем текст сообщения из i18n
             let oBundle = this.getView().getModel('i18n').getResourceBundle();
             let sRecipient = this.getView().getModel().getProperty('/recipient/name');
@@ -14,14 +14,15 @@ sap.ui.define([
             MessageToast.show(sMsg);
         },
 
-		onOpenDialog : function () {
+		onOpenDialog: function () {
 			let oView = this.getView();
 
 			// ленивое создание диалога (в момент запуска)
 			if (!this.pDialog) {
 				this.pDialog = Fragment.load({
 					id: oView.getId(),
-					name: "webapp.modules.main.view.HelloDialog"
+					name: "webapp.modules.main.view.HelloDialog",
+					controller: this
 				}).then(function (oDialog) {
 					// присоединить диалог к корню View
 					oView.addDependent(oDialog);
@@ -32,6 +33,11 @@ sap.ui.define([
 			this.pDialog.then(function(oDialog) {
 				oDialog.open();
 			});
-		}
+		},
+
+		onCloseDialog: function () {
+			this.byId('helloDialog').close();
+        }
+        
     });
 });
