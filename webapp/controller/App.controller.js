54efd1cbb7471f27d0c5052fc68b2329b94c60dc
Gevorg Nakhapetyan <ngs22071993@gmail.com>
2020-12-16 13:04:49 +0300

    feat[ui]: i18n added (step 8)

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,8 +1,9 @@
 sap.ui.define([
     'sap/ui/core/mvc/Controller',
     'sap/m/MessageToast',
-    'sap/ui/model/json/JSONModel'
- ], function (Controller, MessageToast, JSONModel) {
+    'sap/ui/model/json/JSONModel',
+    'sap/ui/model/resource/ResourceModel'
+ ], function (Controller, MessageToast, JSONModel, ResourceModel) {
     'use strict';
     return Controller.extend('webapp.controller.App', {
         onInit : function () {
@@ -14,10 +15,22 @@ sap.ui.define([
             };
             let oModel = new JSONModel(oData);
             this.getView().setModel(oModel);
+            // применяем модель i18n к view
+            let i18nModel = new ResourceModel({
+                bundleName: 'webapp.i18n.i18n',
+                supportedLocales: [''],
+                fallbackLocale: ''
+            });
+            this.getView().setModel(i18nModel, 'i18n');
         },
 
         onShowHello : function () {
-           MessageToast.show('Hello World');
+            // читаем текст сообщения из i18n
+            let oBundle = this.getView().getModel('i18n').getResourceBundle();
+            let sRecipient = this.getView().getModel().getProperty('/recipient/name');
+            let sMsg = oBundle.getText('helloMsg', [sRecipient]);
+            // отображаем
+            MessageToast.show(sMsg);
         }
      });
  });
