124e8a09a766e695a27fda620dee2c784a45a5b3
Gevorg Nakhapetyan <ngs22071993@gmail.com>
2020-12-17 19:56:25 +0300

    chore[lib]: creating simple control

---
+++ webapp/lib/ProductRating.js
@@ -0,0 +1,13 @@
+sap.ui.define([
+	'sap/ui/core/Control'
+], function (Control) {
+    'use strict';
+    return Control.extend('webpack.lib.ProductRating', {
+        metadata : {
+        },
+        init : function () {
+        },
+        renderer : function (oRM, oControl) {
+        }
+    });
+});
