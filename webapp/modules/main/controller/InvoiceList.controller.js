5ab8062d4fed12f373ae988190174d5f7013cda2
Gevorg Nakhapetyan <ngs22071993@gmail.com>
2020-12-17 11:41:00 +0300

    feat[ui]: InvoiceList added formatter

--- webapp/modules/main/controller/InvoiceList.controller.js
+++ webapp/modules/main/controller/InvoiceList.controller.js
@@ -1,10 +1,13 @@
 sap.ui.define([
 	'sap/ui/core/mvc/Controller',
-	'sap/ui/model/json/JSONModel'
-], function (Controller, JSONModel) {
+    'sap/ui/model/json/JSONModel',
+    'mainmodule/model/formatter'
+], function (Controller, JSONModel, formatter) {
 	'use strict';
 
 	return Controller.extend('mainmodule.controller.InvoiceList', {
+        
+        formatter: formatter,
 
 		onInit : function () {
 			var oViewModel = new JSONModel({
